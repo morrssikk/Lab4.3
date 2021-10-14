82888038b6310db08726a9370ca0e7049ae9ecfd
morrssikk <91206206+morrssikk@users.noreply.github.com>
2021-10-14 13:54:27 +0300

    Добавьте файлы проекта.

---
+++ Lab4.3/Lab4.3.cpp
@@ -0,0 +1,44 @@
+﻿/// Lab4.3.cpp
+// < Шарафан Максим >
+// Лабораторна робота No 4.3
+// Табуляція функції, заданої формулою: функція з параметрами.
+// Варіант 19
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, c, a, b, y;
+
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x < 5 && b != 0)
+			y = a * pow((x + 7), 2) - b;
+		else
+			if (x > 5 && b == 0)
+				y = (x - c * a) / (a * x);
+			else
+				y = x / c;
+
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
